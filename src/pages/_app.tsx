24155c27aee9d9a195f5f7dd16dd6c1563eecc57
victorb999 <victor.b_@hotmail.com>
2023-09-09 10:58:59 -0300

    fix: title tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,20 +3,14 @@ import { AnimateSharedLayout } from "framer-motion";
 import "../styles/globals.css";
 
 import Layout from "../components/Layout";
-import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        <title>My new cool app</title>
-      </Head>
-      <AnimateSharedLayout>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </AnimateSharedLayout>
-    </>
+    <AnimateSharedLayout>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AnimateSharedLayout>
   );
 }
 
